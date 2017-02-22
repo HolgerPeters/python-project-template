f96c3a3ea3352a15c36ff6efe5065af8e6e29d99
Holger Peters <email@holger-peters.de>
2017-02-22 20:20:37 +0100

    Add more testing package dependencies

--- {{cookiecutter.module}}/setup.py
+++ {{cookiecutter.module}}/setup.py
@@ -8,5 +8,9 @@ pytest_runner = ['pytest-runner'] if needs_pytest else []
 setup(name="{{cookiecutter.module}}",
       use_scm_version=True,
       setup_requires=['setuptools_scm'] + pytest_runner,
-      tests_require=['pytest', 'pytest-cov'],
+      tests_require=['pytest',
+                     'pytest-cov',
+                     'pytest-flake8',
+                     'hypothesis-pytest',
+                     'hypothesis'],
       packages=['{{cookiecutter.module}}'])
